94ab2d7007a748fa1990303c888ffe5229ef1a59
Inacio Barboza De Lima <InacioBrwI@hotmail.com>
2023-07-10 18:41:10 -0300

    Primeiro commit do app de cinema

--- cinema_app_flutter/lib/HomePage.dart
+++ cinema_app_flutter/lib/HomePage.dart
@@ -9,9 +9,29 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Column(children: [
-
-          ],),
+          child: Column(
+            children: [
+              Padding(padding: const EdgeInsets.symmetric(
+                vertical: 18,
+                horizontal: 20
+                ),
+                child: Row(children: [
+                  Column(children: [
+                    Text(
+                      "Olá Inácio",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
